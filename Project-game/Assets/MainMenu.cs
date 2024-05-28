0b53e3a588e40a951ad5aec403a8fa791ea5b743
Alexandros Papadas <sperako948@gmail.com>
2024-05-28 10:56:58 +0300

    Main Menu (functional)

---
+++ Project-game/Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
