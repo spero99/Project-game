d0b96776d271295ca5f7b1317bc31ccb243ab9ef
Alexandros Papadas <sperako948@gmail.com>
2024-08-06 20:30:16 +0300

    fixed healthpacks
    fixed healthpacks (not more moving on its own ,created a prefab

--- Project-game/Assets/Scripts/Health/HealthPack.cs
+++ Project-game/Assets/Scripts/Health/HealthPack.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
 
 public class HealthPack : MonoBehaviour
@@ -7,14 +8,14 @@ public class HealthPack : MonoBehaviour
     [SerializeField] private float healthValue;
 
 
-    private void onTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Player")  //collision.gameObject.tag == "Player"
+        if(collision.transform.tag == "Player")  //collision.gameObject.tag == "Player"
         {
             Debug.Log("Dont touch me");
 
             collision.GetComponent<Health>().AddHealth(healthValue);
-            gameObject.SetActive(false);
+            gameObject.SetActive(false);    
         }
     }
 }
